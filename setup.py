142761b3fe46c32cf0ecde77a16f2e6c679aee26
Emil Brinch Holm <37075561+EBHolm@users.noreply.github.com>
2023-12-04 15:29:15 +0100

    PyPI continuous integration workflow (#1)
    * Now installs with pyproject.toml
    
    * Added __init__.py to subdirs
    
    * Added PyPI uploading workflow
    
    * No longer require mpi4py
    
    * Bumped version to 23.0.1

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='prospect',
-    version='23.0.0',
-    packages=['prospect'],
-    install_requires=['numpy>=1.17.0', 'scipy>=1.5', 'PyYAML>=5.1', 'GetDist>=1.3.1', 'mpi4py'],
-    entry_points={
-        'console_scripts': [
-            'prospect=prospect.run:run_from_shell',
-            'prospect-analyse=prospect.run:analyse_from_shell',
-            'prospect-reanneal=prospect.profile:reanneal_from_shell'
-        ],
-    },
-)
-
-
+setup()
