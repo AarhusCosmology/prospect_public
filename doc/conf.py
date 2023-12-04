800041f95495e4dd76438fa31a08911ff59e312e
EBHolm <emil.brinch@hotmail.com>
2023-12-04 16:21:39 +0100

    Started documentation.

---
+++ doc/conf.py
@@ -0,0 +1,79 @@
+# Project
+project  = 'PROSPECT'
+author   = 'Emil Brinch Holm'
+language = 'en'  # English
+
+# Paths
+html_static_path = ['static']
+html_css_files = ['custom.css']
+exclude_patterns = ['build']
+
+# Extensions
+extensions = [
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
+    'sphinx_tabs.tabs',
+]
+
+# Never hide tab contents
+sphinx_tabs_disable_tab_closing = True
+
+# Theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    # General
+    'display_version'           : False,
+    'logo_only'                 : False,
+    'prev_next_buttons_location': 'both',
+    'style_external_links'      : True,
+    # Navigation
+    'collapse_navigation'        : False,
+    'navigation_depth'           : -1,
+    'sticky_navigation'          : True,
+    'titles_only'                : False,
+}
+
+# Syntax highlighting of code blocks
+import pygments.styles, pygments.token
+def monkeypatch_pygments(name, base_name='default', attrs={}):
+    import importlib, sys
+    base_module = importlib.import_module('.'.join(['pygments', 'styles', base_name]))
+    def name_to_class_name(name):
+        return name.capitalize() + 'Style'
+    base_class = getattr(base_module, name_to_class_name(base_name))
+    attrs['styles'] = getattr(base_class, 'styles', {}) | attrs.pop('styles', {})
+    class_name = name_to_class_name(name)
+    Style = type(class_name, (base_class,), attrs)
+    module = type(base_module)(name)
+    setattr(module, class_name, Style)
+    setattr(pygments.styles, name, module)
+    pygments.styles.STYLE_MAP[name] = f'{name}::{class_name}'
+    sys.modules['.'.join(['pygments', 'styles', name])] = module
+pygments_style = 'concept'
+monkeypatch_pygments(
+    pygments_style,
+    'friendly',
+    {
+        'background_color': '#f6f6f6',
+        'styles': {
+            pygments.token.Comment:       'italic #688F98',
+            pygments.token.Name.Variable: '#d27a0a',
+        },
+    },
+)
+
+# Exclude Python and Bash prompts when copying code blocks
+copybutton_prompt_text = r'>>> |\$ '
+copybutton_prompt_is_regexp = True
+
+# HTML
+html_baseurl           = 'https://AarhusCosmology.github.io/prospect/'
+html_context           = {'display_github': True}
+# html_favicon           = html_static_path[0] + '/favicon.ico'
+html_last_updated_fmt  = None
+# html_logo              = html_static_path[0] + '/logo.png'
+html_scaled_image_link = False
+html_show_copyright    = False
+html_show_sourcelink   = False
+html_show_sphinx       = False
+html_title             = 'PROSPECT Documentation'
---
+++ doc/index.rst
@@ -0,0 +1,38 @@
+.. role:: raw-html(raw)
+   :format: html
+
+.. role:: bolditalic
+   :class: bolditalic
+
+
+
+CONCEPT documentation
+=====================
+
+.. raw:: html
+
+   <table class="docutils field-list" style="font-size:120%">
+   <col class="field-name" />
+   <col class="field-body" />
+   <tbody valign="top">
+   <tr><td class="field-name"><b>Release</b></td><td class="field-body">1.0.1</td></tr>
+   <tr><td class="field-name"><b>GitHub</b></td><td class="field-body"><a class="reference external" href="https://github.com/jmd-dk/concept/">https://github.com/jmd-dk/concept/</a>&emsp;<img src="https://github.com/jmd-dk/concept/workflows/build/badge.svg"/></td></tr>
+   <tr><td class="field-name"><b>Author</b></td><td class="field-body">Emil Brinch Holm, Aarhus University&emsp;<a class="reference external" href="mailto:dakin&#37;&#52;&#48;phys&#46;au&#46;dk">dakin<span>&#64;</span>phys<span>&#46;</span>au<span>&#46;</span>dk</a></td></tr>
+   <tr class="field-even field"><td class="field-name"><b>Date</b></td><td class="field-body">|today|</td></tr>
+   </tbody>
+   </table>
+
+:raw-html:`<font size="+1">This is the documentation for CO<i>N</i>CEPT, the <b>CO</b>smological <b><i>N</b></i>-body <b>C</b>od<b>E</b> in <b>P</b>y<b>T</b>hon</font>`
+
+
+
+Introduction
+------------
+Hi!
+
+
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
